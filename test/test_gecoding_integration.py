0cc58b276cc15cc05ce44e8616c349c2f2df1a1a
Ian Wagner <ian.wagner@stadiamaps.com>
2023-06-08 11:18:22 +0900

    Add simple integration test

---
+++ test/test_gecoding_integration.py
@@ -0,0 +1,25 @@
+import os
+import unittest
+
+import stadiamaps
+
+
+class TestEndNode(unittest.TestCase):
+    def setUp(self):
+        self.configuration = stadiamaps.Configuration()
+        self.configuration.api_key['ApiKeyAuth'] = os.environ["STADIA_API_KEY"]
+
+    def tearDown(self):
+        pass
+
+    def testAutocomplete(self):
+        with stadiamaps.ApiClient(self.configuration) as api_client:
+            # Create an instance of the API class
+            api_instance = stadiamaps.GeocodingApi(api_client)
+            text = 'Põhja pst 27a'  # str | The place name (address, venue name, etc.) to search for.
+
+            # Search and geocode quickly based on partial input.
+            api_response = api_instance.autocomplete(text)
+            self.assertEqual("Estonia", api_response.features[0].properties.country)
+            # print("The response of GeocodingApi->autocomplete:\n")
+            # pprint(api_response)
